fa83edd270ed82adf5f2179373a463a5d8ece51c
Reed James <reedtjames1234@gmail.com>
2019-11-06 12:27:21 -0600

    Merge sort passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,20 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
+
     for i in range(0, len(arr) - 1):
         cur_index = i
-        print(f'current_index: {cur_index}')
 
         smallest_index = cur_index
-        print(f'smallest_index: {smallest_index}')
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # for j in range(cur_index, len(arr)-1):
+
         for j in range(cur_index+1, len(arr)):
-            print(f'heres the j {j}')
+
             if arr[j] < arr[smallest_index]:
-                # if arr[j] < arr[cur_index]:
                 smallest_index = j
-                print(f'smallest_index in j: {smallest_index} ')
-        print(
-            f'test arr: {arr}, arr length: {len(arr)} small num: {arr[smallest_index]}')
-        # TO-DO: swap
-        # new_arr = arr.append(arr[smallest_index])
+
         new_num = arr[cur_index]
         arr[cur_index] = arr[smallest_index]
         arr[smallest_index] = new_num
 
-        print(f'New Num {new_num}')
     return arr
 
 
@@ -32,7 +22,14 @@ def selection_sort(arr):
 
 
 def bubble_sort(arr):
-
+    done = True
+    while done:
+        done = False
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                # swap
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                done = True
     return arr
 
 
@@ -106,4 +103,71 @@ for i in range(0, len(arr) - 1):
 '''
  # TO-DO: swap
     arr[cur_index] = arr[smallest_index]
+'''
+
+'''
+def bubble_sort(arr):
+    done = False
+    while done is False:
+        for i in range(0, len(arr)-1):
+            current_index = i
+            # check position next to itself
+            for j in range(current_index, current_index+1):
+                if arr[j] < arr[j+1]:
+                    # print('test')
+                    new_num = arr[current_index]
+                    arr[current_index] = arr[j]
+                    arr[j] = new_num
+
+        done = True
+    return arr
+'''
+'''
+def bubble_sort(arr):
+    done = False
+    while done is False:
+        for i in range(0, len(arr)-1):
+            current_index = i
+            # check position next to itself
+            for j in range(current_index, current_index+1):
+                if arr[j] < arr[j+1]:
+                    # print('test')
+                    new_num = arr[current_index]
+                    arr[current_index] = arr[j]
+                    arr[j] = new_num
+                    
+
+        done = True
+    return arr
+'''
+
+'''
+def selection_sort(arr):
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        # print(f'current_index: {cur_index}')
+
+        smallest_index = cur_index
+        # print(f'smallest_index: {smallest_index}')
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 loc)
+        # for j in range(cur_index, len(arr)-1):
+        for j in range(cur_index+1, len(arr)):
+            # print(f'heres the j {j}')
+            if arr[j] < arr[smallest_index]:
+                # if arr[j] < arr[cur_index]:
+                smallest_index = j
+                # print(f'smallest_index in j: {smallest_index} ')
+        # print(
+        #     f'test arr: {arr}, arr length: {len(arr)} small num: {arr[smallest_index]}')
+        # TO-DO: swap
+        # new_arr = arr.append(arr[smallest_index])
+        new_num = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = new_num
+
+        # print(f'New Num {new_num}')
+    return arr
+
 '''
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,47 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# TO-DO: complete the help function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    for i in range(0, elements):
+        if len(arrA) == 0:
+            merged_arr[i] = arrB.pop(0)
+        elif len(arrB) == 0:
+            merged_arr[i] = arrA.pop(0)
+        elif arrA[0] < arrB[0]:
+            merged_arr[i] = arrA.pop(0)
+        else:
+            merged_arr[i] = arrB.pop(0)
+
+    print(f'elements: {elements}')
     return merged_arr
 
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
 
-    return arr
+
+def merge_sort(arr):
+    # TO-DO
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
+
+    arrL = merge_sort(left)
+    arrR = merge_sort(right)
+
+    return merge(arrL, arrR)
+
+
+# def merge_sort(arr):
+#     if len(arr) > 1:
+#         left = merge_sort(arr[:len(arr) // 2])
+#         right = merge_sort(arr[len(arr) // 2:])
+
+#         arr = merge(left, right)
+
+#     return arr
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +50,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +59,75 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
+
+
+'''
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    middle = len(arr) //2
+    left = arr[:middle]
+    right = arr[middle:]
+    
+    arrL = merge_sort(left)
+    arrR = merge_sort(right)
+    
+    return merge(arrL, arrR)
+'''
+
+'''
+import random
+
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
+    # TO-DO
+    for i in range(0, elements):
+        if len(arrA) == 0:
+            merged_arr[i] = arrB.pop(0)
+        elif len(arrB) == 0:
+            merged_arr[i] = arrA.pop(0)
+        elif arrA[0] < arrB[0]:
+            merged_arr[i] = arrA.pop(0)
+        else:
+            merged_arr[i] = arrB.pop(0)
+    
+    print('test')
+    print(f'elements: {elements}')
+    return merged_arr
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    middle = len(arr) //2
+    left = arr[:middle]
+    right = arr[middle:]
+    
+    arrL = merge_sort(left)
+    arrR = merge_sort(right)
+    
+    return merge(arrL, arrR)
+
+
+# foo = random.sample(range(12), 5)
+foo = [2, 4, 5, 1, 3, 9, 6]
+# bar = random.sample(range(12), 5)
+# merge(foo, bar)
+merge_sort(foo)
+'''
+
+'''
+elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
+    # TO-DO
+    merged_arr.append(arrA, arrB)
+    print('test')
+    print(f'elements: {elements}')
+    return merged_arr
+
+'''
