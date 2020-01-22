ed6492f0865ca269fbe3d04348d6679e90408361
Reed James <reedtjames1234@gmail.com>
2020-01-22 14:53:18 -0600

    Recursive sorting complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,7 +3,15 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    # print('elements', elements)
+    for i in range(0, elements):
+        if len(arrA) == 0:
+            merged_arr[i] = arrB.pop(0)
+        elif len(arrB) == 0:
+            merged_arr[i] = arrA.pop(0)
+        elif arrA[0] < arrB[0]:
+            merged_arr[i] = arrA.pop(0)
+        else:
+            merged_arr[i] = arrB.pop(0)
     
     return merged_arr
 
@@ -11,8 +19,19 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) <= 1:
+        return arr
+    pivot = len(arr) //2
+    left = arr[:pivot]
+    right = arr[pivot:]
 
-    return arr
+
+    leftArr = merge_sort(left)
+    rightArr = merge_sort(right)
+
+
+    return merge(leftArr, rightArr)
+    # return arr
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -34,6 +53,7 @@ def timsort( arr ):
     return arr
 
 '''
+# print('elements', elements)
 for i in range(0, elements):
         if len(arrA) == 0:
             merged_arr[i] = arrB.pop(0)
@@ -43,4 +63,25 @@ for i in range(0, elements):
             merged_arr[i] = arrA.pop(0)
         else: merged_arr[i] = arrB.pop(0)
     print('merged_arr', merged_arr)
+print('merged_arr', merged_arr)
+'''
+
+'''
+def merge_sort( arr ):
+    # TO-DO
+    if len(arr) <= 1:
+        return arr
+    pivot = len(arr) //2
+    left = arr[:pivot]
+    right = arr[pivot:]
+
+    print('left', left)
+    print('right', right)
+
+    leftArr = merge_sort(left)
+    rightArr = merge_sort(right)
+    print('leftArr', leftArr)
+    print('rightArr', rightArr)
+
+    return merge(leftArr, rightArr)
 '''
